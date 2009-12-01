fc2715f4a83b2176f6b264a273f21d03a4ec5cac
Brendon Walsh <brendonwalsh@niamu.com>
2009-12-01 11:40:49 -0500

    IE hack update

--- header.php
+++ header.php
@@ -18,10 +18,6 @@
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 <!--<![endif]-->
 
-<!--[if lte IE 7]><!-->
-
-<!--<![endif]-->
-
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
 <?php wp_head(); ?>
